2084527ef2e9dd52c4e2cf2b70441628c593932a
Haim Guel Quiroz <hgq_2000@hotmail.com>
2021-12-10 20:58:15 -0600

    Shows modal window and change styles when it appears

--- modal.js
+++ modal.js
@@ -1,1 +1,19 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.modal-button');
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', () => {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+btnCloseModal.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+// for (let item of btnsOpenModal) {
+//   console.log(item);
+// }
