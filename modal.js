7f47df935984374a9eecc8f497f6d1d677e110c1
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-01-19 13:07:21 -0600

    Add comments

--- modal.js
+++ modal.js
@@ -1,7 +1,15 @@
 'use strict';
+
+//Modal body
 const modal = document.querySelector('.modal');
+
+//Background when modal appears
 const overlay = document.querySelector('.overlay');
+
+//X button to close modal window
 const btnCloseModal = document.querySelector('.close-modal');
+
+//Buttons to open modal window
 const btnsOpenModal = document.querySelectorAll('.modal-button');
 
 const openModal = () => {
@@ -14,12 +22,18 @@ function closeModal() {
   overlay.classList.add('hidden');
 }
 
+//Add all buttons the event 'openModal' when it's clicked
 for (let modalButton of btnsOpenModal) {
   modalButton.addEventListener('click', openModal);
 }
 
+//Add closeModal event when  X button is clicked
 btnCloseModal.addEventListener('click', closeModal);
+
+//Add closeModal event when background is clicked
 overlay.addEventListener('click', closeModal);
+
+//When Escape button is clicked and modal isn't hidden, close modal
 document.addEventListener('keydown', event => {
   if (event.key === 'Escape' && !modal.classList.contains('hidden'))
     closeModal();
